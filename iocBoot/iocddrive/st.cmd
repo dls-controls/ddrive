f7365f0680be88a3c8d6697f050536d5cf382a8f
K Lauer <klauer@bnl.gov>
2015-06-17 13:51:18 -0400

    Autosave, ioc stats

--- iocBoot/iocddrive/st.cmd
+++ iocBoot/iocddrive/st.cmd
@@ -5,15 +5,20 @@
 ## Register all support components
 dbLoadDatabase("../../dbd/ddriveTest.dbd",0,0)
 ddriveTest_registerRecordDeviceDriver(pdbbase) 
+
 epicsEnvSet("P", "$(P=MLL:)")
-epicsEnvSet("R", "$(R=DDRIVE:)")
+epicsEnvSet("R", "$(R=ddrive)")
+# Note: if using procServ, setting this manually is unnecessary:
+epicsEnvSet("IOCNAME", "ddrive")
+epicsEnvSet("IOC_PREFIX", "$(P)$(R){IOC:$(IOCNAME)}")
+
 epicsEnvSet("DDRIVE_NET_IP", "$(DDRIVE_NET_IP=192.168.33.2)")
 epicsEnvSet("DDRIVE_NET_PORT", "$(DDRIVE_NET_PORT=4001)")
 epicsEnvSet("AS_PREFIX", "$(P)")
 epicsEnvSet("ASYN_PORT", "$(ASYN_PORT=DDRIVE)")
 
 drvAsynIPPortConfigure("$(ASYN_PORT)","$(DDRIVE_NET_IP):$(DDRIVE_NET_PORT)",0,0,0)
-#
+
 #drvAsynSerialPortConfigure("$(PORT)", "/dev/ttyS0")
 #asynSetOption("$(ASYN_PORT)", -1,"baud",9600)
 #asynSetOption("$(ASYN_PORT)", -1,"bits",8)
@@ -36,12 +41,31 @@ ddriveCreateController("DDRIVE0", "$(ASYN_PORT)", 3, 50, 100)
 asynSetTraceIOMask("DDRIVE0", -1, 255)
 #asynSetTraceIOMask("$(ASYN_PORT)", -1, 255)
 
-dbLoadRecords("$(TOP)/db/motors.db", "P=MLL:ddrive,PORT=DDRIVE0")
+dbLoadRecords("$(TOP)/db/motors.db", "P=$(P)$(R),PORT=DDRIVE0")
 
-#< save_restore.cmd
+set_savefile_path("${TOP}/as/save","")
+set_requestfile_path("$(EPICS_BASE)/as/req")
+set_requestfile_path("${TOP}/as/req")
+
+system("install -m 777 -d ${TOP}/as/save")
+system("install -m 777 -d ${TOP}/as/req")
+
+cd $(TOP)/as/req
+makeAutosaveFiles()
+
+set_pass0_restoreFile("info_positions.sav")
+set_pass0_restoreFile("info_settings.sav")
+
+dbLoadRecords("$(EPICS_BASE)/db/save_restoreStatus.db","P=$(IOC_PREFIX)")
+dbLoadRecords("$(EPICS_BASE)/db/iocAdminSoft.db","IOC=$(IOC_PREFIX)")
+save_restoreSet_status_prefix("$(IOC_PREFIX)")
+# asSetFilename("/cf-update/acf/default.acf")
 
 iocInit()
 
-#create_monitor_set("auto_positions.req", 5, "P=$(AS_PREFIX)")
-#create_monitor_set("auto_settings.req", 30, "P=$(AS_PREFIX)")
+create_monitor_set("info_positions.req", 5, "")
+create_monitor_set("info_settings.req", 30, "")
 
+cd ${TOP}
+dbl > ./records.dbl
+system "cp ./records.dbl /cf-update/$HOSTNAME.$IOCNAME.dbl"
